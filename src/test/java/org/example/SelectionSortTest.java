c7b16ab31b71b6d64223bb29ffd2dcb75aa92e09
Christian165293 <christian.bourquin05@gmail.com>
2025-01-29 10:09:45 -0600

    The Test class has now been implemented so this short Project has now been finished.This was done as practice while reading Grokking Algorithms

--- src/test/java/org/example/SelectionSortTest.java
+++ src/test/java/org/example/SelectionSortTest.java
@@ -5,10 +5,57 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SelectionSortTest {
+    @Test
+    public void testSelectionSortWithUnsortedArray() {
+        int[] input = {64, 25, 12, 22, 11};
+        int[] expected = {11, 12, 22, 25, 64};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
 
     @Test
-    void selectionSort() {
+    public void testSelectionSortWithSortedArray() {
+        int[] input = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 4, 5};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
 
+    @Test
+    public void testSelectionSortWithReversedArray() {
+        int[] input = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
 
+    @Test
+    public void testSelectionSortWithSingleElementArray() {
+        int[] input = {1};
+        int[] expected = {1};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSelectionSortWithEmptyArray() {
+        int[] input = {};
+        int[] expected = {};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSelectionSortWithDuplicatesArray() {
+        int[] input = {4, 4, 3, 2, 1, 1};
+        int[] expected = {1, 1, 2, 3, 4, 4};
+        SelectionSort selectionSortObj = new SelectionSort();
+        selectionSortObj.selectionSort(input);
+        assertArrayEquals(expected, input);
     }
 }
